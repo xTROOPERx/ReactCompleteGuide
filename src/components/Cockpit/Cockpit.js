a86d4229384dc84904791b058560f85cabaf4573
Trooper Kristianson <barefootraveler@gmail.com>
2019-09-04 12:42:16 -0400

    videos added to 145

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,56 @@
+import React, {useEffect, useRef, useContext } from 'react';
+
+import classes from './Cockpit.module.css';
+import AuthContext from '../../context/auth-context';
+
+const Cockpit = (props) => {
+  const toggleBtnRef= useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
+
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // setTimeout(() => {
+    //   alert('Saved Data To Cloud!');
+    // }, 1000);
+    toggleBtnRef.current.click();
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
+  
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
+
+  const assignedClasses = [];
+  let btnClass = '';
+  if(props.showPersons) {
+      btnClass =classes.red;
+  }
+  if (props.personsLength <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.personsLength <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return(
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is actually working</p>
+      <button ref={toggleBtnRef}
+        className={btnClass}
+        onClick={props.clicked}>
+          Toggle Person
+      </button>
+      <button onClick={authContext.login}>Log in</button>
+    </div>    
+  );
+};
+
+export default React.memo(Cockpit);
---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,50 @@
+import React, {PureComponent} from 'react';
+
+import Person from './Person/Person';
+class Persons extends PureComponent {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Persons.js] shouldComponentUpdate');
+  //   if (
+  //     nextProps.Persons !== this.props.Persons || 
+  //     nextProps.changed !== this.props.changed || 
+  //     nextProps.clicked !== this.props.clicked 
+  //     ) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return {message: 'Snapshot Message!'}
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Perons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log('[Persons.js] ...rendering');
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
+}
+
+
+export default Persons;
